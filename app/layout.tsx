477d67d96763ab0729471a5f8a4ff48e39b0d167
Blake Preston <note2bp@protonmail.com>
2024-01-17 10:39:03 -0800

    indentation changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,63 +12,63 @@ import GtagScriptInit from 'components/Gtag/GtagScriptInit';
 // font
 import { Vesper_Libre, Arimo, } from 'next/font/google';
 const fontHead = Vesper_Libre({
-  subsets: ['latin'],
-  weight: ["400"],
-  variable: '--font-head',
+   subsets: ['latin'],
+   weight: ["400"],
+   variable: '--font-head',
 });
 const fontBody = Arimo({
-  subsets: ['latin'],
-  weight: ["400", "500", "600", "700"],
-  variable: '--font-body',
+   subsets: ['latin'],
+   weight: ["400", "500", "600", "700"],
+   variable: '--font-body',
 });
 
 export const metadata: Metadata = {
-  title: 'Hillside Evangelical',
-  description: 'Hillside Evangelical Church, Greystones',
+   title: 'Hillside Evangelical',
+   description: 'Hillside Evangelical Church, Greystones',
 }
 
 export default function RootLayout({
-  children,
+   children,
 }: {
-  children: React.ReactNode,
+   children: React.ReactNode,
 }) {
 
-  return (
-    <html lang="en" className={`${fontHead.variable} ${fontBody.variable}`}>
+   return (
+      <html lang="en" className={`${fontHead.variable} ${fontBody.variable}`}>
 
-      <head>
-      </head>
+         <head>
+         </head>
 
-      <body>
+         <body>
 
-        {/* Landing */}
-        <Landing/>
+            {/* Landing */}
+            <Landing />
 
-        {/* Evernything else */}
-        <div className='h-screen bg-grey-200'>
-          {/* w is the screen - scrollbar gutter */}
-          <div className="absolute top-0 left-0 w-[calc(100vw-14px)] font-body text-grey-950 bg-grey-200 selection:bg-ax1-300">
-      
-            <Header>
-              Hillside Evangelical Church, Greystones
-            </Header>
-            
-            <div className='flex justify-center items-start z-0 bg-inherit'>
-              <div className='nav-scroll sticky top-0 hidden lg:block w-64 h-screen overflow-y-auto'>
-                <Nav/>
-              </div>
-              {children}
-              <div className='nav-scroll sticky top-0 hidden xl:block w-64 h-screen overflow-y-auto'>
-                {/* <Nav/> */}
-              </div>
+            {/* Evernything else */}
+            <div className='h-screen bg-grey-200'>
+               {/* w is the screen - scrollbar gutter */}
+               <div className="absolute top-0 left-0 w-[calc(100vw-14px)] font-body text-grey-950 bg-grey-200 selection:bg-ax1-300">
+
+                  <Header>
+                     Hillside Evangelical Church, Greystones
+                  </Header>
+
+                  <div className='flex justify-center items-start z-0 bg-inherit pr-64'>
+                     <div className='nav-scroll sticky top-0 hidden lg:block w-64 h-screen overflow-y-auto'>
+                        <Nav />
+                     </div>
+                     {children}
+                     {/* <div className='nav-scroll sticky top-0 hidden xl:block w-64 h-screen overflow-y-auto'>
+                        
+                     </div> */}
+                  </div>
+
+               </div>
             </div>
+            {/* <GtagScriptInit /> */}
+         </body>
 
-          </div>
-        </div>
-        {/* <GtagScriptInit /> */}
-      </body>
-      
 
-    </html>
-  )
+      </html>
+   )
 }
