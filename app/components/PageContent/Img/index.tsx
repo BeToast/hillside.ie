7c3c320a97503edf4762edf40ee26bc6b920e7f6
Blake Preston <note2bp@protonmail.com>
2024-01-17 14:19:47 -0800

    indent

--- app/components/PageContent/Img/index.tsx
+++ app/components/PageContent/Img/index.tsx
@@ -3,62 +3,62 @@ import { afterH1Class } from '../Hr/static';
 
 /*
 
-  Image to be called in document as £ [file]$[position]
-  
-  example:
-    £ click.webp?right
+   Image to be called in document as £ [file]$[position]
+   
+   example:
+      £ click.webp?right
 
 */
 
 const Img: React.FC<{
-  slugPath: string,
-  fileName: string,
-  position: string, //'center' | 'right',
-  previousEl: El,
+   slugPath: string,
+   fileName: string,
+   position: string, //'center' | 'right',
+   previousEl: El,
 }> = ({
-  slugPath,
-  fileName,
-  position,
-  previousEl,
+   slugPath,
+   fileName,
+   position,
+   previousEl,
 }) => {
-  //position input invalid input checker
-  if(position !== 'center' && position !== 'right') {
-    console.warn('The string is not "right" or "center" defaulting to "center"');
-    position = 'center';
-  }
+      //position input invalid input checker
+      if (position !== 'center' && position !== 'right') {
+         console.warn('The string is not "right" or "center" defaulting to "center"');
+         position = 'center';
+      }
 
-  //img element
-  const img: React.ReactElement = <>
-    <img
-      className='rounded-lg shadow-inner border border-grey-800'
-      src={`/content/${slugPath+fileName}`}
-      alt={fileName}
-    />
-  </>
+      //img element
+      const img: React.ReactElement = <>
+         <img
+            className='rounded-lg shadow-inner border border-grey-800'
+            src={`/content/${slugPath + fileName}`}
+            alt={fileName}
+         />
+      </>
 
-  const getConditionalStyles = (previousEl: string, position: string): string => {
-    var styles: string[] = [];
+      const getConditionalStyles = (previousEl: string, position: string): string => {
+         var styles: string[] = [];
 
-    //just previousEl
-    if(previousEl === 'H1') styles.push(`${afterH1Class} pt-0 mt-0`)
+         //just previousEl
+         if (previousEl === 'H1') styles.push(`${afterH1Class} pt-0 mt-0`)
 
-    //just position
-    if(position === 'center') styles.push('float-none')
-    else if(position === 'right') styles.push('float-right pt-12 ml-8 pb-4')
+         //just position
+         if (position === 'center') styles.push('float-none')
+         else if (position === 'right') styles.push('float-right pt-12 ml-8 pb-4')
 
-    //combos
-    // if(previousEl === 'H2' && position === 'right') styles.push('') 
-
-
-    return styles.join(' ');
-  }
+         //combos
+         // if(previousEl === 'H2' && position === 'right') styles.push('') 
 
 
-  return (<>
-    <div className={`Img px-4 ${getConditionalStyles(previousEl, position)}`}>
-      {img}
-    </div>
-  </>);
-}
+         return styles.join(' ');
+      }
+
+
+      return (<>
+         <div className={`Img px-4 ${getConditionalStyles(previousEl, position)}`}>
+            {img}
+         </div>
+      </>);
+   }
 
 export default Img;
