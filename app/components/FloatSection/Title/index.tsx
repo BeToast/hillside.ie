d2e7954730e938c6063e1c0dda237bac0e8d13a2
Blake Preston <note2bp@protonmail.com>
2024-02-04 18:19:10 -0800

    pausing project to do google's UX course

--- app/components/FloatSection/Title/index.tsx
+++ app/components/FloatSection/Title/index.tsx
@@ -9,7 +9,7 @@ const Title: React.FC<{
    // const zIndex = (isFront ? 'z-20' : 'z-0');
 
    return(<>
-      <div className={`absolute pl-[3.8rem] -top-8 text-6xl font-head tracking-wider subpixel-antialiased ${textColor}`}>
+      <div className={`absolute pl-[3.8rem] -top-12 text-6xl font-head tracking-wider subpixel-antialiased ${textColor}`}>
          {title}
       </div>
       {isDark ? <></> : <span className="text-6xl font-head text-transparent">{title}</span>}
